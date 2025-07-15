6c3e95add376b4c1787528006ad672879523e67e
Jaymeid <jamespawson64@gmail.com>
2025-07-15 15:09:56 +0100

    Remove components from home screen

--- src/routes/home/HomePage.tsx
+++ src/routes/home/HomePage.tsx
@@ -2,10 +2,6 @@ import Hero from "./components/Hero";
 import { Main } from "../../components/styled/Main.styled";
 import Products from "./components/Products";
 import CustomerReviews from "./components/CustomerReviews";
-import StyledButtonComponent from "../../components/styledButton/StyledButton";
-import ProgressBar from "../../components/progressBar/ProgressBar";
-import GoalField from "../../components/goalField/GoalField";
-import BigDropDown from "../../components/bigDropDown/BigDropDown";
 
 export default function HomePage() {
   return (
@@ -13,16 +9,6 @@ export default function HomePage() {
       <Hero />
       <Products />
       <CustomerReviews />
-      <StyledButtonComponent to={"hello"}>Hello</StyledButtonComponent>
-      <ProgressBar progress={10} />
-      <GoalField />
-      <BigDropDown
-        title="Click to Expand"
-        content={<div>Here is the expanded content!</div>}
-        expandedHeight={300}
-      >
-        <h1>Hello</h1>
-      </BigDropDown>
     </Main>
   );
 }
