4edb7c400b28c219400c838b4ee6e6cdee3182a3
yashvas <yashvas15@gmail.com>
2025-07-16 03:58:14 -0700

    Pushed

--- src/routes/savings/SavingsPage.tsx
+++ src/routes/savings/SavingsPage.tsx
@@ -1,3 +1,9 @@
-export default function SavingsPage () {
-    return <h1>hello world</h1>;
+import './Savings.css';
+export default function SavingsPage() {
+    return (
+        <div className="page-container">
+            <h1>Savings Tracker</h1>
+            <div className="circle"></div> {/* This is the circle */}
+        </div>
+    );
 }
